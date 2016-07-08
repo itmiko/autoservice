8de8308c79361e6e0243af4a46a2ecb0a223a522
itmiko <mypost.mk@gmail.com>
2016-07-08 23:21:50 +0300

    add main menu

--- template_bootstrap/js/common (copy).js
+++ template_bootstrap/js/common (copy).js
@@ -40,7 +40,7 @@ $(document).ready(function() {
 	//Навигация по Landing Page
 	//$(".top_mnu") - это верхняя панель со ссылками.
 	//Ссылки вида <a href="#contacts">Контакты</a>
-	// $(".top_mnu").navigation();
+	$(".top_mnu").navigation();
 
 	//Добавляет классы дочерним блокам .block для анимации
 	//Документация: http://imakewebthings.com/jquery-waypoints/
