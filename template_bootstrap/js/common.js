6d3648ae5833b45d996126ca7880013065f7ddb9
itmiko <mypost.mk@gmail.com>
2016-07-14 12:19:49 +0300

    add menu

--- template_bootstrap/js/common.js
+++ template_bootstrap/js/common.js
@@ -1,4 +1,24 @@
 $(document).ready(function() {
+	$(".mc_item_wrap ul").each(function() {
+		$(this).after("<div class='mc_item_wrap_af'></div>");
+	});
+
+
+	$(".mc_toggle").click(function() {
+		if($(this).parent().parent().children("ul").is(":visible")) {
+			$(this).parent().parent().children("ul").slideUp();
+			$(this).parent().parent().children(".mc_item_wrap_af").hide();
+
+		} else {
+			$(".mc_wrap .mc_item_wrap > ul, .mc_item_wrap_af").hide();
+			$(".mc_item_wrap").removeClass("active");
+			$(this).parent().parent().addClass("active");
+			$(this).parent().parent().children("ul").slideToggle();
+			$(this).parent().parent().children(".mc_item_wrap_af").slideToggle();
+		}
+	});
+
+
 
 	//Цели для Яндекс.Метрики и Google Analytics
 	$(".count_element").on("click", (function() {
